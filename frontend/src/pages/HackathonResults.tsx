d5f25ced7fc710e94c9be31264be2cb5311939ee
JorgeCSH <jorge.cumminz@gmail.com>
2025-10-18 21:21:12 -0300

    feat: add HackathonResults function Still cooking

--- frontend/src/pages/HackathonResults.tsx
+++ frontend/src/pages/HackathonResults.tsx
@@ -1,1 +1,11 @@
-// This is a dummy file created to do dummy file stuff
+import { useHackatons } from '@/hooks/useHackatons'
+import { useSubmissions } from '@/hooks/useSubmissions'
+import { useParams, Link, useNavigate } from 'react-router-dom'
+import { LiveProvider, LivePreview } from 'react-live'
+import { Button } from '@/components/ui/button'
+
+const HackathonResults = () => {
+   // Yes
+}
+
+export { HackathonResults }
